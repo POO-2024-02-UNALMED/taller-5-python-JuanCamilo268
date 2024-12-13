48bba21a12b7be162e7676850663ee21a518da30
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-13 17:00:32 -0500

    Update zoologico.py

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,5 +1,18 @@
-class Zoologico:
+class Zoologico:  
     def __init__(self, nombre = None, ubicacion = None):
         self._nombre = nombre
         self._ubicacion = ubicacion
         self._zonas = []
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    def setUbicacion(self, ub):
+        self._ubicacion = ub
+    
+    def getNombre(self):
+        return self._nombre
+    def getUbicacion(self):
+        return self._ubicacion
+    
+    def cantidadTotalAnimales(self):
+        for zona in self._zonas:
