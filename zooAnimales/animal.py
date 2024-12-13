67aa07a646b291bf18c3cfec939ec093d499f07f
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-13 17:00:25 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -5,5 +5,27 @@ class Animal:
         self._edad = edad
         self._habitat = habitat
         self._genero = genero
-        self.zona = zona
+        self._zona = zona
         Animal._totalAnimales +=1
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    def setEdad(self, edad):
+        self._edad = edad
+    def setHabitat(self, habitat):
+        self._habitat = habitat
+    def setGenero(self, genero):
+        self._genero = genero
+    def setZona(self, zona):
+        self._zona = zona
+
+    def getNombre(self):
+        return self._nombre
+    def getEdad(self):
+        return self._edad
+    def getHabitat(self):
+        return self._habitat
+    def getGenero(self):
+        return self._genero
+    def getZona(self):
+        return self._zona
