e7d4584322fa9e7874bdb993948627a9669f7189
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-13 21:26:14 -0500

    Update reptil.py

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -4,13 +4,14 @@ class Reptil(Animal):
     _listado = []
     serpientes = 0
     iguanas = 0
-    def __init__(self, nombre = None, edad = None, habitat = None, genero = None, color = None, largo = None):
+    def __init__(self, nombre = None, edad = None, habitat = None, genero = None, color = None, largo = None, zona = None):
         self._nombre = nombre
         self._edad = edad
         self._habitat = habitat
         self._genero = genero
         self._colorEscamas = color
         self._largoCola = largo
+        self._zona = zona
         Reptil._listado.append(self)
         super()
     
