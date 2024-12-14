09b6b1e0d7d7117b2799837d092c708c9e10c7fc
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-13 21:26:20 -0500

    Update mamifero.py

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -4,13 +4,14 @@ class Mamifero(Animal):
     _listado = []
     caballos = 0
     leones = 0
-    def __init__(self, nombre = None, edad = None, habitat = None, genero = None, pelaje = None, patas = None):
+    def __init__(self, nombre = None, edad = None, habitat = None, genero = None, pelaje = None, patas = None, zona = None):
         self._nombre = nombre
         self._edad = edad
         self._habitat = habitat
         self._genero = genero
         self._pelaje = pelaje
         self._patas = patas
+        self._zona = zona
         Mamifero._listado.append(self)
         super()
     
