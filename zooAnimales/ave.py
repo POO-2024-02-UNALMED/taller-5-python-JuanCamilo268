eb105464e49303230ca6275e50548b68efb48e47
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-13 20:48:55 -0500

    Update ave.py

--- zooAnimales/ave.py
+++ zooAnimales/ave.py
@@ -26,11 +26,11 @@ class Ave(Animal):
     def movimiento(self):
         return "volar"
     
-    def crearHalcon(self, nombre, edad, genero):
+    def crearHalcon(nombre, edad, genero):
         piyoto = Ave(nombre, edad, "montanas", genero, "cafe glorioso")
         Ave.halcones += 1
         return piyoto
-    def crearAguila(self, nombre, edad, genero):
+    def crearAguila(nombre, edad, genero):
         murica = Ave(nombre, edad, "montanas", genero, "blanco y amarillo")
         Ave.aguilas += 1
         return murica
